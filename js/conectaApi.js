00ca65dfd6a184f852497c97609554a440ecc012
Henrique Velardo <Henrique>
2023-02-22 20:00:39 -0300

    Criando requisição a API do tipo POST, para cadastro de vídeos no servidor json

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -2,11 +2,32 @@
 async function listaVideos() {
     const conexao =  await fetch("http://localhost:3000/videos")
     const conexaoConvertida = await conexao.json()
-    console.log(conexaoConvertida)
+    
     return conexaoConvertida
 }
 
+async function criaVideos(titulo, descricao, url, imagem){
+    const conexao = await fetch ("http://localhost:3000/videos", {
+        method: "POST", 
+        headers: {
+            "Content-type" : "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil vizualizações`,
+            url: url,
+            imagem: imagem
+        })
+    })
+
+    const conexaoConvertida = await conexao.json()
+    console.log(conexaoConvertida, "show")
+    return conexaoConvertida
+}
+
+
 //Objeto exportador de funções
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideos
 }
